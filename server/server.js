08280ab6a631337e50a47df740f62e48726db8a5
Tony K Tan <tankwantiong@gmail.com>
2016-10-19 00:14:43 -0700

    Set up server side code to run on heroku (first try)

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 /* Database */
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+// mongoose.connect('mongodb://localhost/test');
+// mongoose.connect('mongodb://heroku_hktz3v8j:1nd1hk0940mnn5cgij44v3crja@ds061676.mlab.com:61676/heroku_hktz3v8j');
+mongoose.connect(process.env.MONGODB_URI);
 
 var Pellet = mongoose.model('Pellet', { 
   text: String,
@@ -26,6 +28,7 @@ var express = require('express');
 var expressJSON = require('express-json'); 
 var bodyParser = require('body-parser');
 var server = express();
+server.use(express.static(__dirname + "/build"));
 server.use(expressJSON()); 
 server.use(bodyParser()); 
 
@@ -44,13 +47,12 @@ server.get('/pellets', function (req, res) {
 });
 
 server.post('/pellets', function (req, res) {
-  console.log('receiving pellets to add to database body', req.body);
   var pelletText = req.body.text;
   makePellet(pelletText);
   res.set(defaultCorsHeaders);
   res.send('new pellet added!');
 });
 
-server.listen(3000, function () {
-  console.log('Pellets server listening on port 3000!');
+server.listen(process.env.PORT || 8080, function () {
+  console.log(`Pellets server listening on port ${process.env.PORT || 8080}!`);
 });
